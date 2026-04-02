cf07f5f9c2a4c22251b58fc257dccf52523a653a
Nathan Musoke <nathan.musoke@gmail.com>
2026-04-02 10:56:47 -0600

    refactor: Move index order benchmarks to group

--- benchmark/indexorder.jl
+++ benchmark/indexorder.jl
@@ -1,14 +1,22 @@
 using BenchmarkTools
 
-resol = 10_000
-nfields = 1
+suite = BenchmarkGroup()
 
-a_long_first = rand(resol, nfields)
-r_long_first = range(1.0, 10.0, resol)
-b_long_first = @benchmark abs2.($a_long_first) ./ $r_long_first
+suite["spherical"] = BenchmarkGroup()
 
-a_long_second = rand(nfields, resol)
-r_long_second = reshape(r_long_first, 1, resol)
-b_long_second = @benchmark abs2.($a_long_second) ./ $r_long_second
+begin
+    resol_r = 10_000
+    nfields = 1
 
-@show judge(minimum(b_long_first), minimum(b_long_second))
+    a_long_first = rand(resol_r, nfields)
+    r_long_first = range(1.0, 10.0, resol_r)
+    suite["spherical"]["r first"] = @benchmarkable abs2.($a_long_first) ./ $r_long_first
+
+    a_long_second = rand(nfields, resol_r)
+    r_long_second = reshape(r_long_first, 1, resol_r)
+    suite["spherical"]["field first"] = @benchmarkable abs2.($a_long_second) ./
+        $r_long_second
+end
+
+results = run(suite)
+@show judge(minimum(results["spherical"]["r first"]), minimum(results["spherical"]["field first"]))
