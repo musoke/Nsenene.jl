868eb467b0b2cbbc97ce684f2189466abd19fb29
nathan musoke <nathan.musoke@gmail.com>
2026-04-02 15:28:52 -0400

    opt: Swap the cylindrical R and z indices
    FFTs are much faster when they are over the first coordinate. Make z
    first.

--- benchmark/indexorder.jl
+++ benchmark/indexorder.jl
@@ -1,14 +1,109 @@
+using AbstractFFTs
 using BenchmarkTools
+using FFTW
+using LinearAlgebra
 
-resol = 10_000
-nfields = 1
+suite = BenchmarkGroup()
 
-a_long_first = rand(resol, nfields)
-r_long_first = range(1.0, 10.0, resol)
-b_long_first = @benchmark abs2.($a_long_first) ./ $r_long_first
+suite["spherical"] = BenchmarkGroup()
+begin
+    resol_r = 10_000
+    nfields = 1
 
-a_long_second = rand(nfields, resol)
-r_long_second = reshape(r_long_first, 1, resol)
-b_long_second = @benchmark abs2.($a_long_second) ./ $r_long_second
+    a_long_first = rand(resol_r, nfields)
+    r_long_first = range(1.0, 10.0, resol_r)
+    suite["spherical"]["r first"] = @benchmarkable abs2.($a_long_first) ./ $r_long_first
 
-@show judge(minimum(b_long_first), minimum(b_long_second))
+    a_long_second = rand(nfields, resol_r)
+    r_long_second = reshape(r_long_first, 1, resol_r)
+    suite["spherical"]["field first"] = @benchmarkable abs2.($a_long_second) ./
+        $r_long_second
+end
+
+begin
+    resol_R = 1024
+    resol_z = 2 * resol_R
+
+    rho_R_first = randn(resol_R, resol_z)
+    rho_z_first = permutedims(rho_R_first, (2, 1))
+
+    @assert size(rho_R_first) == (resol_R, resol_z)
+    @assert size(rho_z_first) == (resol_z, resol_R)
+
+    fftplan_R_first = AbstractFFTs.plan_rfft(rho_R_first, 2)
+    fftplan_z_first = AbstractFFTs.plan_rfft(rho_z_first, 1)
+
+    # fftplan_R_first_patient = AbstractFFTs.plan_rfft(copy(rho_R_first), 2; flags=FFTW.PATIENT)
+    # fftplan_z_first_patient = AbstractFFTs.plan_rfft(copy(rho_z_first), 1; flags=FFTW.PATIENT)
+
+    suite["cylindrical"]["FFT"] = BenchmarkGroup()
+
+    suite["cylindrical"]["FFT"]["R first estimate"] = @benchmarkable $fftplan_R_first *
+        $rho_R_first
+    suite["cylindrical"]["FFT"]["z first estimate"] = @benchmarkable $fftplan_z_first *
+        $rho_z_first
+
+    # suite["cylindrical"]["FFT"]["R first patient"] = @benchmarkable $fftplan_R_first_patient * $rho_R_first
+    # suite["cylindrical"]["FFT"]["z first patient"] = @benchmarkable $fftplan_z_first_patient * $rho_z_first
+
+    suite["cylindrical"]["FFT"]["R first unplanned"] = @benchmarkable rfft(rho_R_first, 2)
+    suite["cylindrical"]["FFT"]["z first unplanned"] = @benchmarkable rfft(rho_z_first, 1)
+
+    rhok_R_first = fftplan_R_first * rho_R_first
+    rhok_z_first = fftplan_z_first * rho_z_first
+
+    ifftplan_R_first = AbstractFFTs.plan_irfft(rhok_R_first, resol_z, 2)
+    ifftplan_z_first = AbstractFFTs.plan_irfft(rhok_z_first, resol_z, 1)
+
+    suite["cylindrical"]["IFFT"]["R first estimate"] = @benchmarkable $ifftplan_R_first *
+        $rhok_R_first
+    suite["cylindrical"]["IFFT"]["z first estimate"] = @benchmarkable $ifftplan_z_first *
+        $rhok_z_first
+
+    kz = AbstractFFTs.rfftfreq(resol_z)
+    D = Tridiagonal(randn(resol_R, resol_R))
+
+    function operator_inverse_R_first(rhok, D, kz, Phik)
+        for (i, kz) in enumerate(kz)
+            Dk = D - UniformScaling(kz^2)
+            Dk[end, end - 1] = 0
+            Dk[end, end] = 1
+
+            Phik[:, i] .= Dk \ rhok[:, i]
+        end
+
+        return Phik
+    end
+
+    function operator_inverse_z_first(rhok, D, kz, Phik)
+        for (i, kz) in enumerate(kz)
+            Dk = D - UniformScaling(kz^2)
+            Dk[end, end - 1] = 0
+            Dk[end, end] = 1
+
+            Phik[i, :] .= Dk \ rhok[i, :]
+        end
+
+        return Phik
+    end
+
+    inv_R_first = operator_inverse_R_first(rhok_R_first, D, kz, similar(rhok_R_first))
+    inv_z_first = operator_inverse_z_first(rhok_z_first, D, kz, similar(rhok_z_first))
+
+    @assert all((inv_R_first ≈ transpose(inv_z_first)))
+
+    suite["cylindrical"]["invert tridiagonal"]["R first"] = @benchmarkable operator_inverse_R_first(
+        rhok_R_first, D, kz, similar(rhok_R_first)
+    )
+    suite["cylindrical"]["invert tridiagonal"]["z first"] = @benchmarkable operator_inverse_z_first(
+        rhok_z_first, D, kz, similar(rhok_z_first)
+    )
+end
+
+results = run(suite)
+
+@show judge(
+    minimum(results["spherical"]["r first"]), minimum(results["spherical"]["field first"])
+)
+
+results
--- test/derivatives.jl
+++ test/derivatives.jl
@@ -15,7 +15,7 @@ m_ = 1:nfields
     @test size(dR1) == (resol, resol)
 
     @testset "first derivative of r should be 1" begin
-        d = dR1 * p.R / Cylindrical.dR_element(p)
+        d = dR1 * transpose(p.R) / Cylindrical.dR_element(p)
 
         @test d[begin:end] ≈ ones(resol) atol = 1e-10
     end
@@ -26,7 +26,7 @@ m_ = 1:nfields
     @test size(dR2) == (resol, resol)
 
     @testset "second derivative of r should vanish" begin
-        d = dR2 * p.R / Cylindrical.dR_element(p)^2
+        d = dR2 * transpose(p.R) / Cylindrical.dR_element(p)^2
 
         @test d[(begin + 1):(end - 1)] ≈ zeros(resol - 2) atol = 1e-10
         @test_broken d[begin] ≈ 0.0 atol = 1e-10
@@ -34,7 +34,7 @@ m_ = 1:nfields
     end
 
     @testset "second derivative of r^2 should be 2" begin
-        d = dR2 * (p.R .^ 2) / Cylindrical.dR_element(p)^2
+        d = dR2 * transpose(p.R .^ 2) / Cylindrical.dR_element(p)^2
 
         @test d[(begin + 1):(end - 1)] ≈ 2 * ones(resol - 2) atol = 1e-10
         @test_broken d[begin] ≈ 2.0 atol = 1e-10
